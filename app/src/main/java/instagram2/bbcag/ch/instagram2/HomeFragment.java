32e1a369d3ee931ff73c9ae72c431be3d0b6a6eb
SuterSabrina <sabrina.suter@yahoo.com>
2018-06-20 10:04:28 +0200

    richtiger stand

--- app/src/main/java/instagram2/bbcag/ch/instagram2/HomeFragment.java
+++ app/src/main/java/instagram2/bbcag/ch/instagram2/HomeFragment.java
@@ -1,40 +1,19 @@
 package instagram2.bbcag.ch.instagram2;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.ListView;
 import android.view.View;
+import android.view.ViewGroup;
 
-import java.util.ArrayList;
-
+import instagram2.bbcag.ch.instagram2.R;
 
 public class HomeFragment extends Fragment {
-    private static final String TAG = "Home_Fragment";
-
-    private ListView mListView;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        mListView = (ListView) view.findViewById(R.id.listView);
-
-        ArrayList<Card> list = new ArrayList<>();
-
-        list.add(new Card("drawable://" + R.drawable.pool, "pool"));
-        list.add(new Card("drawable://" + R.drawable.pool, "pool"));
-        list.add(new Card("drawable://" + R.drawable.pool, "pool"));
-        list.add(new Card("drawable://" + R.drawable.pool, "pool"));
-        list.add(new Card("drawable://" + R.drawable.pool, "pool"));
-        list.add(new Card("drawable://" + R.drawable.pool, "pool"));
-
-//        CustomListAdapter adapter = new CustomListAdapter(this, R.layout.activity_main, list);
-//        mListView.setAdapter(adapter);
-
-
-        return view;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
