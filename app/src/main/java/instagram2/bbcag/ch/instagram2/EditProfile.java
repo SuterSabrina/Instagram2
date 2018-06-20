181cd733433773a677c65fbb515e7418af59185c
SuterSabrina <sabrina.suter@yahoo.com>
2018-06-20 09:33:54 +0200

    new things on edit profile

--- app/src/main/java/instagram2/bbcag/ch/instagram2/EditProfile.java
+++ app/src/main/java/instagram2/bbcag/ch/instagram2/EditProfile.java
@@ -3,6 +3,8 @@ package instagram2.bbcag.ch.instagram2;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class EditProfile extends AppCompatActivity {
 
