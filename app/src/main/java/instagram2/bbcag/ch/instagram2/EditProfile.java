3d993625adb1bd2587c094c60059044f37d96263
SuterSabrina <sabrina.suter@yahoo.com>
2018-06-20 08:10:13 +0200

    new activity

---
+++ app/src/main/java/instagram2/bbcag/ch/instagram2/EditProfile.java
@@ -0,0 +1,14 @@
+package instagram2.bbcag.ch.instagram2;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_profile);
+    }
+}
