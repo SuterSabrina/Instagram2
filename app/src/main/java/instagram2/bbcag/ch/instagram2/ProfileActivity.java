7ed3fd57818096f59f7a4daad3f2599253a1fae8
SuterSabrina <sabrina.suter@yahoo.com>
2018-06-25 15:23:43 +0200

    added new classes

--- app/src/main/java/instagram2/bbcag/ch/instagram2/ProfileActivity.java
+++ app/src/main/java/instagram2/bbcag/ch/instagram2/ProfileActivity.java
@@ -5,28 +5,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.view.View.OnClickListener;
-import android.widget.EditText;
 
-public class ProfileAcitivity extends AppCompatActivity implements View.OnClickListener{
+
+public class ProfileActivity extends AppCompatActivity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        System.out.print("Hello IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
         Button button = (Button) findViewById(R.id.editBnt);
         button.setOnClickListener(this);
     }
 
+    @Override
     public void onClick(View v) {
-        System.out.print("c,asdmalcmmc");
-        Intent intent = new  Intent(this, EditProfilePicture.class);
+        System.out.print("Hello DDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+        Intent intent = new Intent(this, EditProfilePicture.class);
         startActivity(intent);
 
     }
