c66ca900d2193af5315d9687059cb674e2f95845
SuterSabrina <sabrina.suter@yahoo.com>
2018-06-26 08:32:47 +0200

    test db insert

--- app/src/main/java/instagram2/bbcag/ch/instagram2/Login/MainActivity.java
+++ app/src/main/java/instagram2/bbcag/ch/instagram2/Login/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import instagram2.bbcag.ch.instagram2.Feed.FeedActivity;
 import instagram2.bbcag.ch.instagram2.R;
 import instagram2.bbcag.ch.instagram2.SignUp.SignUpActivity;
+import instagram2.bbcag.ch.instagram2.User.UserJDBCDao;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -30,6 +31,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 startActivity(intent1);
                 break;
             case R.id.noLoginButton:
+                UserJDBCDao userJDBCDao = new UserJDBCDao();
+                userJDBCDao.addNewUser("Hallihallo","niilaxx1232","sabrina.suter@gmx.com","123458");
                 Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
                 startActivity(intent);
                 break;
